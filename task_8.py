2df4859bb0172ff2e1a50e935f610acef7135419
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-02-16 21:54:34 +0700

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,4 +1,5 @@
 user_login = input('Login: ')
 password_1 = input('Password: ')
 password_new = input('New password: ')
-print(f'User {user_login} has changed the password to {password_new}')
+password_1 = password_new
+print(f'User {user_login} has changed the password to {password_1}')
