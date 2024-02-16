06e9e08d1e42612b7719cfad053ba4c7d096e348
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-02-16 21:56:31 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,8 +1,8 @@
-j = ['Номер рейса: ', 'Название авиакомании(на русском): ', 'Название авиакомпании(на английском): ',
-     'Город прилёта(на русском): ', 'Город прилёта(на английском): ']
-all_infrmation = []
-for i in j:
-    infrmation = input(i)
-    all_infrmation.append(infrmation)
-print(f'Заканчивается посадка на рейс {all_infrmation[0]} {all_infrmation[1]} до {all_infrmation[3]}')
-print(f'This is the final boarding call for {all_infrmation[2]} flight {all_infrmation[0]} to {all_infrmation[4]}')
+songs = []
+print('Введите плей-лист папы:')
+for i in range(5):
+    song = input()
+    songs.append(song)
+print('Плей-лист мамы:')
+for j in range(5):
+    print(songs[::-1][j])
